9166bd87c56f61d5a714ced881c880112a09abce
NMonear1 <monearnoah@gmail.com>
2025-10-07 22:10:01 -0400

    starter code with some tables added in the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,51 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users ON DELETE CASCADE;
+DROP TABLE IF EXISTS accounts ON DELETE CASCADE;
+DROP TABLE IF EXISTS transactions ON DELETE CASCADE;
+DROP TABLE IF EXISTS transfers ON DELETE CASCADE;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
+  firstname text NOT NULL,
+  lastname text NOT NULL,
+  email text NOT NULL UNIQUE,
   username text NOT NULL UNIQUE,
-  password text NOT NULL
+  password text NOT NULL,
+  phonenumber text NOT NULL
 );
+
+CREATE TABLE accounts (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  type TEXT NOT NULL, 
+  account_number TEXT UNIQUE NOT NULL,
+  balance NUMERIC(12,2) DEFAULT 0.00,
+  created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+  amount NUMERIC(12,2) NOT NULL, 
+  type TEXT NOT NULL,               
+  description TEXT,
+  created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE transfers (
+  id SERIAL PRIMARY KEY,
+  from_account_id INT REFERENCES accounts(id),
+  to_account_id INT REFERENCES accounts(id),
+  amount NUMERIC(12,2) NOT NULL CHECK (amount > 0),
+  created_at TIMESTAMP DEFAULT now()
+);
+ 
+
+ -- fun idea for session management if needed later
+
+-- CREATE TABLE sessions (
+--   id SERIAL PRIMARY KEY,
+--   user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+--   token TEXT NOT NULL UNIQUE,
+--   created_at TIMESTAMP DEFAULT now(),
+--   expires_at TIMESTAMP NOT NULL
+-- );
