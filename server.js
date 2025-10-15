cfc6c54ef70a79db527d9c4e40b8477fb01f930f
NMonear1 <monearnoah@gmail.com>
2025-10-14 21:33:18 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from "#app";
 import db from "#db/client";
+import cors from "cors";
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -8,3 +9,10 @@ await db.connect();
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
