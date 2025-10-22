c6d09f14c797dfbb6ac2da76325763027f117b96
NMonear1 <monearnoah@gmail.com>
2025-10-21 21:01:27 -0400

    updated account routes

--- api/accounts.js
+++ api/accounts.js
@@ -1,22 +1,31 @@
 import express from "express";
 const router = express.Router();
 
-import { getAccount } from "#db/queries/accounts";
+import { getAccount, getUserAccounts } from "#db/queries/accounts";
 import requireUser from "#middleware/requireUser";
 
-
 //get account from the token
-router
-  .route("/")
-  .get(requireUser, async (req, res) => {
-    try {
+router.route("/").get(requireUser, async (req, res) => {
+  try {
     console.log("GET /account");
-    const account = await getAccount(req.user.id);
-    res.send(account);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: error.message });
-    }
-  })
+    const accounts = await getUserAccounts(req.user.id);
+    res.send(accounts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+});
 
-  export default router;
+router.route("/:type").get(async (req, res) => {
+  try {
+    const { type } = req.params;
+    const accounts = await getUserAccounts(req.user.id);
+    const filteredAccount = accounts.filter((acc) => acc.type === type);
+    res.send(filteredAccount);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+});
+
+export default router;
--- db/queries/accounts.js
+++ db/queries/accounts.js
@@ -1,40 +1,63 @@
 import db from "#db/client";
 
-export async function createAccount({user_id, type, account_number, routing_number, balance, created_at}) {
-    const sql = `
+export async function createAccount({
+  user_id,
+  type,
+  account_number,
+  routing_number,
+  balance,
+  created_at,
+}) {
+  const sql = `
     INSERT INTO accounts
         (user_id, type, account_number, routing_number, balance, created_at)
         VALUES
         ($1, $2, $3, $4, $5, $6)
         RETURNING *
         `;
-        const {
-        rows: [account],
-        } = await db.query(sql, [user_id, type, account_number, routing_number, balance, created_at]);
-        return account;
+  const {
+    rows: [account],
+  } = await db.query(sql, [
+    user_id,
+    type,
+    account_number,
+    routing_number,
+    balance,
+    created_at,
+  ]);
+  return account;
 }
 
-export async function getAccounts () { 
-    const sql = `
+export async function getAccounts() {
+  const sql = `
     SELECT *
     FROM accounts
     `;
-    const { rows: accounts } = await db.query(sql);
-    return accounts;
+  const { rows: accounts } = await db.query(sql);
+  return accounts;
 }
 
 export async function getAccount(id) {
-    const sql = `
+  const sql = `
     SELECT *
     FROM accounts
     WHERE id = $1
     `;
-    const { rows: [account] } = await db.query(sql, [id]);
-    return account;
+  const {
+    rows: [account],
+  } = await db.query(sql, [id]);
+  return account;
 }
 
-export async function updateAccount({ id, user_id, type, account_number, balance, created_at }) {
-    const sql = `
+export async function updateAccount({
+  id,
+  user_id,
+  type,
+  account_number,
+  balance,
+  created_at,
+}) {
+  const sql = `
     UPDATE accounts SET
         user_id = $2,
         type = $3,
@@ -44,16 +67,37 @@ export async function updateAccount({ id, user_id, type, account_number, balance
     WHERE id = $1
     RETURNING *;
     `;
-    const { rows: [account] } = await db.query(sql, [id, user_id, type, account_number, balance, created_at]);
-    return account;
+  const {
+    rows: [account],
+  } = await db.query(sql, [
+    id,
+    user_id,
+    type,
+    account_number,
+    balance,
+    created_at,
+  ]);
+  return account;
 }
 
 export async function deleteAccount(id) {
-    const sql = `
+  const sql = `
     DELETE FROM accounts
     WHERE id = $1
     RETURNING *;
     `;
-    const { rows: [account] } = await db.query(sql, [id]);
-    return account;
-};
+  const {
+    rows: [account],
+  } = await db.query(sql, [id]);
+  return account;
+}
+
+export async function getUserAccounts(user_id) {
+  const sql = `
+    SELECT *
+    FROM accounts
+    WHERE user_id = $1
+    `;
+  const { rows: accounts } = await db.query(sql, [user_id]);
+  return accounts;
+}
